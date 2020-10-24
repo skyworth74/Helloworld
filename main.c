eed3083c5c62c7d4f83e2861e34cac7ad4160aa2
skyworth <hushiyong@sohu.com>
2020-10-24 16:39:14 +0800

    print message 1

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(void)
     printf("--                                    --\r\n");
     printf("--                                    --\r\n");
     printf("--                                    --\r\n");
+    printf("1\r\n");
     printf("----------------------------------------\r\n");
     return 1;
 }
